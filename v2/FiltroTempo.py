33ea35f81a88321517b22f9f7167bab4aeed60c3
Igor Correa <igor.a.correa@hotmail.com>
2019-04-10 20:10:23 -0300

    alteracoes de filtro finais

--- v2/FiltroTempo.py
+++ v2/FiltroTempo.py
@@ -1,6 +1,8 @@
 import numpy as np #mexer com arrays
 import pandas as pd #mexer com tabelas
 import time #mexer com tempo
+import glob, os #mexer com arquivos
+
 
 def tempoStrToInt(tempo): #função para transformar horario em int
     milesimo = 1000
@@ -34,42 +36,65 @@ def find_nearest_idx(array, value): # função para achar o valor mais perto em 
 inicio = time.time() #variavel para contar o tempo do arquivo
 
 
-#caminho = "H:/TCC/ArquivoFinal/v2/Consolidado.csv"
-caminho = "H:/TCC/ArquivosFiltrados/v2/FiltradoDia20180702.csv"
 periodicidade = 2 * 1000 #define a periodicidade em que o arquivo vai ser filtrado
 horarioInicio = '09:04:00.000' #define horario de inicio para começar a pegar os arquivos
 horarioInicioInt = tempoStrToInt(horarioInicio) #traduz o horario inicial
-horarioFim = '18:00:00.000' #define horario de fim para terminar de pegar os arquivos
+horarioFim = '18:00:01.000' #define horario de fim para terminar de pegar os arquivos
 horarioFimInt = tempoStrToInt(horarioFim) #traduz o horario fim
 
 
-arquivo = pd.read_csv(caminho) #le o arquivo
-arquivo = arquivo[arquivo['horario_negociacao'] > horarioInicio] #filtra para começar apenas em um horario certo
-arquivo['horario_int'] = arquivo.apply(lambda row: tempoStrToInt(row['horario_negociacao']), axis=1) #cria coluna do valor inteiro do horario
+caminhoOrigem = "H:/TCC/ArquivosFiltrados/v2/" #define caminho onde vai pegar o arquivo
+caminhoDestino = "H:/TCC/ArquivosFiltrados/v3/" #define caminho onde vai ser jogado os arquivos
 
 
-colunaHorarios = np.array(arquivo['horario_int'].values.tolist()) #cria coluna de referencia dos horarios do arquivo
-
-
-arquivoFinal = pd.DataFrame(columns=arquivo.columns) #cria variavel para consolidar os valores mais perto
-
-
-for horaReferencia in range(horarioInicioInt, horarioFimInt, periodicidade): #desde a hora de inicio ate a hora final com step de periodicidade
-    maisPerto = find_nearest(colunaHorarios, horaReferencia) #acha o horario mais perto
-    colunaQuantidade = arquivo[arquivo['horario_int'] == maisPerto] #filtra o arquivo só para os casos que tem o mesmo horario
-    qntTotal = int(colunaQuantidade['quantidade_negociada'].sum()) #soma todas as quantidades de mesmo horario
-    idx = find_nearest_idx(colunaHorarios, horaReferencia) #index da linha do valor mais perto
-    linhaAdicionar = arquivo.iloc[idx] #cria variavel da linha a ser adicionada
-    linhaAdicionar['quantidade_negociada'] = qntTotal #muda o valor da quantidade para o total
-    arquivoFinal = arquivoFinal.append(linhaAdicionar) #adiciona no arquivo final
-# =============================================================================
-#     if horaReferencia == tempoStrToInt('10:00:00.000'):
-#         break
-# =============================================================================
-# =============================================================================
-#     if colunaQuantidade.shape[0] > 1:
-#         break
-# =============================================================================
+os.chdir(caminhoOrigem) #caminho da pasta de importação
+for file in glob.glob("*.csv"): #pega arquivos com final TXT
+    
+    
+    inicioParcial = time.time() #variavel para contar o tempo do arquivo
+    
+    
+    arquivo = pd.read_csv(file) #le o arquivo
+    arquivo = arquivo[arquivo['horario_negociacao'] > horarioInicio] #filtra para começar apenas em um horario certo
+    arquivo['horario_int'] = arquivo.apply(lambda row: tempoStrToInt(row['horario_negociacao']), axis=1) #cria coluna do valor inteiro do horario
+    
+    
+    colunaHorarios = np.array(arquivo['horario_int'].values.tolist()) #cria coluna de referencia dos horarios do arquivo
+    
+    
+    arquivoFinal = pd.DataFrame(columns=arquivo.columns) #cria variavel para consolidar os valores mais perto
+    
+    
+    for horaReferencia in range(horarioInicioInt, horarioFimInt, periodicidade): #desde a hora de inicio ate a hora final com step de periodicidade
+        maisPerto = find_nearest(colunaHorarios, horaReferencia) #acha o horario mais perto
+        colunaQuantidade = arquivo[arquivo['horario_int'] == maisPerto] #filtra o arquivo só para os casos que tem o mesmo horario
+        qntTotal = int(colunaQuantidade['quantidade_negociada'].sum()) #soma todas as quantidades de mesmo horario
+        colunaQuantidade = None
+        idx = find_nearest_idx(colunaHorarios, horaReferencia) #index da linha do valor mais perto
+        linhaAdicionar = arquivo.iloc[idx] #cria variavel da linha a ser adicionada
+        linhaAdicionar['quantidade_negociada'] = qntTotal #muda o valor da quantidade para o total
+        arquivoFinal = arquivoFinal.append(linhaAdicionar) #adiciona no arquivo final
+    # =============================================================================
+    #     if horaReferencia == tempoStrToInt('10:00:00.000'):
+    #         break
+    # =============================================================================
+    # =============================================================================
+    #     if colunaQuantidade.shape[0] > 1:
+    #         break
+    # =============================================================================
+    
+    
+    diaDoArquivo = file[11:19]
+    arquivoFinal.to_csv(caminhoDestino + "MaisFiltradoDia" + diaDoArquivo + ".csv", index=None, header=True) #exporta arquivo do dia WDO
+        
+    
+    colunaHorarios = None
+    arquivoFinal = None
+    arquivo = None #limpa a variavel arquivo
+    
+    
+    fim = time.time() - inicioParcial #contabiliza o tempo do arquivo
+    print("Arquivo do dia " + diaDoArquivo + " filtrado em {:.3f}".format(fim) + " segundos") #mostra o tempo que demorou pra filtrar o arquivo
 
 
 fim = time.time() - inicio #contabiliza o horario
--- v2/TratamentoWDO.py
+++ v2/TratamentoWDO.py
@@ -2,12 +2,44 @@ import pandas as pd #mexer com tabelas
 import glob, os #mexer com arquivos
 import time #mexer com tempo
 from bizdays import Calendar #usar feriados
+import numpy as np #mexer com arrays
+
+###########################   FUNÇÕES   ##############################
+def tempoStrToInt(tempo): #função para transformar horario em int
+    milesimo = 1000
+    horas = int(tempo[:2]) * 3600 * milesimo
+    minutos = int(tempo[3:5]) * 60 * milesimo
+    segundos = int(tempo[6:8]) * milesimo
+    milesimos = int(tempo[-3:])
+    return horas + minutos + segundos + milesimos
+    
+def tempoIntToStr(tempo): #função para transformar int para horario
+    milesimo = 1000
+    horas = int(tempo//(3600 * milesimo))
+    tempo -= horas * (3600 * milesimo)
+    minutos = int(tempo//(60 * milesimo))
+    tempo -= minutos * (60 * milesimo)
+    segundos = int(tempo//milesimo)
+    tempo -= segundos * milesimo
+    return '{:02d}'.format(horas) + ":" + '{:02d}'.format(minutos) + ":" + '{:02d}'.format(segundos) + "." + '{:03d}'.format(tempo)
+    
+def find_nearest(array, value): # função para achar o valor mais perto em um array
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+def find_nearest_idx(array, value): # função para achar o valor mais perto em um array
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+#######################################################################
 
 
-###############################VARIAVEIS###############################
+
+############################   VARIAVEIS   ############################
 HDexterno = "H:/" #coloquei isso porque fica mudando o nome do diretorio
 pastaArquivosDescompactados = HDexterno + "TCC/ArquivosDescompactados/"
-pastaArquivosFiltrados = HDexterno + "TCC/ArquivosFiltrados/v2/"
+pastaArquivosFiltrados = HDexterno + "TCC/ArquivosFiltrados/v3/"
 pastaArquivoConsolidado = HDexterno + "TCC/ArquivoFinal/v2/"
 inicioTotal = time.time() #variavel pra contar o tempo
 
@@ -23,13 +55,19 @@ vencimentoWDO = pd.read_csv(HDexterno + "TCC/vencimentoWDO.csv") #le o arquivo c
 feriados = pd.read_csv(HDexterno + "TCC/feriadosBR.csv") #le o arquivo com os feriados BRs
 holidays = feriados['Data'] #pega só as datas
 cal = Calendar(holidays=holidays, weekdays=['Sunday', 'Saturday']) #adiciona no calendario para poder controlar
+
+periodicidade = 2 * 1000 #define a periodicidade em que o arquivo vai ser filtrado
+horarioInicio = '09:04:00.000' #define horario de inicio para começar a pegar os arquivos
+horarioInicioInt = tempoStrToInt(horarioInicio) #traduz o horario inicial
+horarioFim = '18:00:01.000' #define horario de fim para terminar de pegar os arquivos
+horarioFimInt = tempoStrToInt(horarioFim) #traduz o horario fim
 #######################################################################
 
 
 
 
 
-#################################FILTRO################################
+##############################   FILTRO   #############################
 os.chdir(pastaArquivosDescompactados) #caminho da pasta de importação
 for file in glob.glob("*.txt"): #pega arquivos com final TXT
     
@@ -37,12 +75,14 @@ for file in glob.glob("*.txt"): #pega arquivos com final TXT
     inicioParcial = time.time() #variavel para contar o tempo do arquivo
     
     
+    #################### filtro basicos ####################
     arquivo = pd.read_csv(file, sep=';', skiprows=1, header=None, names=indexTotal) #le o arquivo
     arquivo.drop(arquivo.tail(1).index, inplace=True) #tira ultima linha do arquivo
     arquivo = arquivo.filter(items=indexFiltrado) #pega apenas as colunas que precisamos
     arquivo['simbolo_instrumento'] = arquivo['simbolo_instrumento'].str.strip() #tira os espaços em branco
     
     
+    #################### filtro do WDO e vencimento ####################
     diaDoArquivo = file[8:16] #pega a data do arquivo
     mesDoDia = int(diaDoArquivo[4:6]) #pega o mes da data do movimento
     anoDoDia = int(diaDoArquivo[:4]) #pega o ano da data do movimento
@@ -58,9 +98,27 @@ for file in glob.glob("*.txt"): #pega arquivos com final TXT
     arquivo = arquivo[arquivo['simbolo_instrumento'].str.contains('WDO' + letraCorreta)] #filtra o arquivo por WDO e a letra do vencimento
     
     
-    arquivo.to_csv(pastaArquivosFiltrados + "FiltradoDia" + diaDoArquivo + ".csv", index=None, header=True) #exporta arquivo do dia WDO
+    ################### filtro de horario ###################
+    arquivo = arquivo[arquivo['horario_negociacao'] > horarioInicio] #filtra para começar apenas em um horario certo
+    arquivo['horario_int'] = arquivo.apply(lambda row: tempoStrToInt(row['horario_negociacao']), axis=1) #cria coluna do valor inteiro do horario
+    colunaHorarios = np.array(arquivo['horario_int'].values.tolist()) #cria coluna de referencia dos horarios do arquivo
+    arquivoFinal = pd.DataFrame(columns=arquivo.columns) #cria variavel para consolidar os valores mais perto
+    for horaReferencia in range(horarioInicioInt, horarioFimInt, periodicidade): #desde a hora de inicio ate a hora final com step de periodicidade
+        maisPerto = find_nearest(colunaHorarios, horaReferencia) #acha o horario mais perto
+        colunaQuantidade = arquivo[arquivo['horario_int'] == maisPerto] #filtra o arquivo só para os casos que tem o mesmo horario
+        qntTotal = int(colunaQuantidade['quantidade_negociada'].sum()) #soma todas as quantidades de mesmo horario
+        colunaQuantidade = None
+        idx = find_nearest_idx(colunaHorarios, horaReferencia) #index da linha do valor mais perto
+        linhaAdicionar = arquivo.iloc[idx] #cria variavel da linha a ser adicionada
+        linhaAdicionar['quantidade_negociada'] = qntTotal #muda o valor da quantidade para o total
+        arquivoFinal = arquivoFinal.append(linhaAdicionar) #adiciona no arquivo final
+    
+    
+    arquivoFinal.to_csv(pastaArquivosFiltrados + "MaisFiltradoDia" + diaDoArquivo + ".csv", index=None, header=True) #exporta arquivo do dia WDO
         
     
+    colunaHorarios = None #limpa coluna de referencia
+    arquivoFinal = None #limpa arquivo final
     arquivo = None #limpa a variavel arquivo
     
     
@@ -72,25 +130,27 @@ for file in glob.glob("*.txt"): #pega arquivos com final TXT
 
 
 
-################################CONSOLIDADO############################
-arquivoConsolidado = pd.DataFrame(columns=indexFiltrado) #cria variavel consolidada
-
-
-os.chdir(pastaArquivosFiltrados) #caminho da pasta de importação
-for file in glob.glob("*.csv"): #pega arquivos com final CSV
-    
-    
-    inicioParcial = time.time() #variavel para contar o tempo do arquivo
-    
-    
-    arquivoConsolidado = arquivoConsolidado.append(pd.read_csv(file)) #adiciona o novo arquivo no final do arquivo consolidado
-    
-    
-    fim = time.time() - inicioParcial #contabiliza o tempo do arquivo
-    print("Arquivo do dia " + file[11:19] + " consolidado em {:.3f}".format(fim) + " segundos") #mostra o tempo que demorou pra filtrar o arquivo
-
-
-arquivoConsolidado.to_csv(pastaArquivoConsolidado + "Consolidado.csv", index=None, header=True) #exporta arquivo do dia WDO
+#############################   CONSOLIDADO   #########################
+# =============================================================================
+# arquivoConsolidado = pd.DataFrame(columns=indexFiltrado) #cria variavel consolidada
+# 
+# 
+# os.chdir(pastaArquivosFiltrados) #caminho da pasta de importação
+# for file in glob.glob("*.csv"): #pega arquivos com final CSV
+#     
+#     
+#     inicioParcial = time.time() #variavel para contar o tempo do arquivo
+#     
+#     
+#     arquivoConsolidado = arquivoConsolidado.append(pd.read_csv(file)) #adiciona o novo arquivo no final do arquivo consolidado
+#     
+#     
+#     fim = time.time() - inicioParcial #contabiliza o tempo do arquivo
+#     print("Arquivo do dia " + file[11:19] + " consolidado em {:.3f}".format(fim) + " segundos") #mostra o tempo que demorou pra filtrar o arquivo
+# 
+# 
+# arquivoConsolidado.to_csv(pastaArquivoConsolidado + "Consolidado.csv", index=None, header=True) #exporta arquivo do dia WDO
+# =============================================================================
 #######################################################################
 
 
